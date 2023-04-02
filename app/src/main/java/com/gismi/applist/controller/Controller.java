d84757c36faed2783d91770610ff393f00439b5a
Gismii <gisme.guimaraes@gmail.com>
2023-04-02 00:38:31 -0300

    arquitetura mvc

---
+++ app/src/main/java/com/gismi/applist/controller/Controller.java
@@ -0,0 +1,4 @@
+package com.gismi.applist.controller;
+
+public class Controller {
+}
--- app/src/main/java/com/gismi/applist/view/MainActivity.java
+++ app/src/main/java/com/gismi/applist/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.gismi.applist;
+package com.gismi.applist.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.gismi.applist.R;
 
 public class MainActivity extends AppCompatActivity {
 
