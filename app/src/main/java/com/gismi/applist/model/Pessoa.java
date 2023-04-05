8dc2e148611fec3aeb075cf02fc91d025f173577
Gismii <gisme.guimaraes@gmail.com>
2023-04-04 23:40:55 -0300

    Cria class pessoa

--- app/src/main/java/com/gismi/applist/model/Pessoa.java
+++ app/src/main/java/com/gismi/applist/model/Pessoa.java
@@ -1,4 +1,8 @@
 package com.gismi.applist.model;
 
 public class Pessoa {
+
+    public String nome;
+    public String sobrenome;
+    public String numeroTelefone;
 }
--- app/src/main/java/com/gismi/applist/view/MainActivity.java
+++ app/src/main/java/com/gismi/applist/view/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.gismi.applist.R;
+import com.gismi.applist.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
     }
 }
