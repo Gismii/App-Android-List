39c9911b6ffa85207290c06205a81d736083c591
Gismii <gisme.guimaraes@gmail.com>
2023-04-05 21:49:46 -0300

    getters e setters

--- app/src/main/java/com/gismi/applist/model/Pessoa.java
+++ app/src/main/java/com/gismi/applist/model/Pessoa.java
@@ -2,7 +2,48 @@ package com.gismi.applist.model;
 
 public class Pessoa {
 
-    public String nome;
-    public String sobrenome;
-    public String numeroTelefone;
+    private String primeiroNome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String numeroTelefone;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+
+
+    public Pessoa(){
+
+    }
+
+
 }
--- app/src/main/java/com/gismi/applist/view/MainActivity.java
+++ app/src/main/java/com/gismi/applist/view/MainActivity.java
@@ -11,11 +11,29 @@ public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
 
+    String dadosPessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Gismi");
+        pessoa.setSobrenome("Guimaraes");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setNumeroTelefone("48996409838");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobre nome: ";
+        dadosPessoa += pessoa.getSobrenome();
+        dadosPessoa += "Curso desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += "Numero Telefone: ";
+        dadosPessoa += pessoa.getNumeroTelefone();
+
+
+
     }
 }
